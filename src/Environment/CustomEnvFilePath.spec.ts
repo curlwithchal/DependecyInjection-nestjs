6f2be5a5809201c8f9854d4f20efc2f5af97e7e6
curlwithchal <chaltalikidev@gmail.com>
2023-10-09 06:54:38 +0800

    CUSTOM ENV FILE PATH

---
+++ src/Environment/CustomEnvFilePath.spec.ts
@@ -0,0 +1,28 @@
+import { Test } from '@nestjs/testing';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import ServiceConfigurationEnvironment from './ServiceConfigurationEnvironment';
+
+describe('Custom Environment Env File Path', () => {
+  let serviceConfiguration: ServiceConfigurationEnvironment;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env.Bar',
+        }),
+      ],
+      providers: [ServiceConfigurationEnvironment],
+    }).compile();
+
+    serviceConfiguration = moduleRef.get(ServiceConfigurationEnvironment);
+  });
+
+  test('test custom env path file', () => {
+    const configService = new ConfigService();
+    expect(configService.get('BAR')).toBe('BAR');
+    expect(configService.get('BAR')).toEqual('BAR');
+    expect(configService.get('BAR')).not.toBe('FOOBAR');
+    expect(configService.get('BAR')).not.toEqual('FOOBAR');
+  });
+});
