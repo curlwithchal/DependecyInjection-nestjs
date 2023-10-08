db45aa2cbaa52cccad0d6975083c11e4f312bb03
curlwithchal <chaltalikidev@gmail.com>
2023-10-09 07:24:19 +0800

    Ignore Env File Path Test

---
+++ src/Environment/IgnoreEnvFileConfiguration.spec.ts
@@ -0,0 +1,29 @@
+import { Test } from '@nestjs/testing';
+import { ConfigModule} from '@nestjs/config';
+import ServiceConfigurationEnvironment from './ServiceConfigurationEnvironment';
+import Configuration from './Configuration/Configuration';
+
+describe('Custom Environment Env File Path', () => {
+  let serviceConfiguration: ServiceConfigurationEnvironment;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          ignoreEnvFile: true,
+        }),
+      ],
+      providers: [ServiceConfigurationEnvironment],
+    }).compile();
+
+    serviceConfiguration = moduleRef.get(ServiceConfigurationEnvironment);
+  });
+
+  test('test ignore env path file', () => {
+    expect(Configuration().fooBar).toBe('fooBar');
+    expect(Configuration().fooBar).toEqual('fooBar');
+    expect(Configuration().fooBar).not.toBe('Production');
+    expect(Configuration().fooBar).not.toBe('Hello World');
+    expect(serviceConfiguration).toBeDefined();
+  });
+});
