b83f356633541215714e76801e4a19ded13c89d7
curlwithchal <chaltalikidev@gmail.com>
2023-10-09 06:38:43 +0800

    Read Configuration Env Test

---
+++ src/Environment/LoadEnvironment.spec.ts
@@ -0,0 +1,24 @@
+import { Test } from '@nestjs/testing';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import ServiceConfigurationEnvironment from './ServiceConfigurationEnvironment';
+import * as process from "process";
+
+describe('Load Environment', () => {
+  let serviceConfiguration: ServiceConfigurationEnvironment;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
+      providers: [ServiceConfigurationEnvironment],
+    }).compile();
+
+    serviceConfiguration = moduleRef.get(ServiceConfigurationEnvironment);
+  });
+
+  test('test env Get environment', () => {
+    const config = new ConfigService();
+    expect(config.get('FOOBAR')).toEqual('foobar');
+    expect(config.get('FOOBAR')).toEqual(process.env.FOOBAR);
+    expect(serviceConfiguration).toBeDefined();
+  });
+});
