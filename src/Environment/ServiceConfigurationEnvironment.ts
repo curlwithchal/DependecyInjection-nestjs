5856a06f81b53a6223eea12a458a68b4fc18b81d
curlwithchal <chaltalikidev@gmail.com>
2023-10-09 06:39:17 +0800

    Config Service DI

---
+++ src/Environment/ServiceConfigurationEnvironment.ts
@@ -0,0 +1,10 @@
+import {Inject, Injectable} from "@nestjs/common";
+import {} from "@nestjs/config";
+
+
+@Injectable()
+export default class ServiceConfigurationEnvironment {
+
+    private readonly configuration: string
+
+}
